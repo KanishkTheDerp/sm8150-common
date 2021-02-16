439afd9077b704091e53afe1a9ca077887ed44ab
Mikhail Naganov <mnaganov@google.com>
2021-02-16 15:00:39 -0800

    sm8150-common: Switch audio HAL to V7
    Bug: 142480271
    Test: N/A
    Change-Id: I6c785e8d2becffa3811ce4cd58b130914d4dc4ab
    Signed-off-by: KanishkTheDerp <kanishkthederp@gmail.com>

--- common.mk
+++ common.mk
@@ -93,8 +93,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
